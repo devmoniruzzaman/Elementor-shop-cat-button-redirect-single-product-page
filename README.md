5ff4793d3beb19f00b5fb48b2343f22538279aaf
Moniruzzaman <144223325+devmoniruzzaman@users.noreply.github.com>
2023-09-08 03:45:00 +0600

    function.php

--- README.md
+++
@@ -1,2 +0,0 @@
-# Elementor-shop-cat-button-redirect-single-product-page
-How to Replace "your-elementor-button-class" with the actual CSS class of the "Quick Add to Cart" button.
---
+++ function.php
@@ -0,0 +1,8 @@
+add_filter('woocommerce_loop_add_to_cart_link', 'custom_quick_add_to_cart_button');
+
+function custom_quick_add_to_cart_button($link) {
+    global $product;
+    $product_link = $product->get_permalink();
+    return '<a href="' . esc_url($product_link) . '" rel="nofollow" class="custom-view">View</a>';
+}
+
